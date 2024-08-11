7de654741b9cf3173d97993c590f2038b854049e
Your Name <you@example.com>
2024-08-11 14:06:55 +0330

    not important

--- project/numgame/script.js
+++ project/numgame/script.js
@@ -18,36 +18,27 @@ checkButton.addEventListener("click", () => {
     [guess.textContent, input.disabled] = ["😃درست بود،برنده شدید", true];
 
     [checkButton.textContent, guess.style.color] = ["مجدد", "#00D000"];
+  } else if (difference < 10 && difference > 0) {[guess.textContent, remainChances.textContent] = ["نزدیک شدی، بیشتر حدس بزن",chance,];
 
-  } else if (difference < 10 && difference > 0) {
-    [guess.textContent, remainChances.textContent] = ["نزدیک شدی، بیشتر حدس بزن",chance,
-    ];
-  } else if (difference < 0 && difference > -10) {
-    [guess.textContent, remainChances.textContent] = ["نزدیک شدی، کمتر حدس بزن",chance,
-    ];
+  }
+   else if (difference < 0 && difference > -10) {[guess.textContent, remainChances.textContent] = ["نزدیک شدی، کمتر حدس بزن",chance,];
   }
 
   //کمتر
-  else if (inputValue > randomNum && inputValue <= 100 && inputValue > 0) {
-    [guess.textContent, remainChances.textContent] = ["کمتر حدس بزن", chance];
+  else if (inputValue > randomNum && inputValue <= 100 && inputValue > 0){[guess.textContent, remainChances.textContent] = ["کمتر حدس بزن", chance];
   }
 
   //بیشتر
-  else if (inputValue < randomNum && inputValue <= 100 && inputValue > 0) {
-    [guess.textContent, remainChances.textContent] = ["بیشتر حدس بزن", chance];
+  else if (inputValue < randomNum && inputValue <= 100 && inputValue > 0) {[guess.textContent, remainChances.textContent] = ["بیشتر حدس بزن", chance];
   }
 
   // عدد خارچ از بازه
   else {
-    [guess.textContent, remainChances.textContent] = [
-      "  گفتم از 1 تا 100 (از شانسات کم کردم که بفهمی)",
-      chance,
-    ];
+    [guess.textContent, remainChances.textContent] = ["  گفتم از 1 تا 100 (از شانسات کم کردم که بفهمی)",chance, ];
   }
   // اتمام شانس
   if (chance == 0) {
-    [checkButton.textContent, input.disabled, inputValue] = ["مجدد", true, ""];
-    [guess.textContent, guess.style.color] = ["😞 متاسفم باختی "+"("+randomNum+")", "#DE0611"];
+    [checkButton.textContent, input.disabled, inputValue] = ["مجدد", true, ""];[guess.textContent, guess.style.color] = [ "😞 متاسفم باختی " + "(" + randomNum + ")","#DE0611", ];
   }
   if (chance < 0) {
     window.location.reload();
